ecfd27aa106f9792d4a6b184daa50cc6bd127ed4
AamerDirar <aamer.idris91@gmail.com>
2024-02-08 10:16:29 +0300

    select fields and relationships

--- app/Filament/Resources/ProductResource.php
+++ app/Filament/Resources/ProductResource.php
@@ -19,6 +19,12 @@ class ProductResource extends Resource
 
     protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
 
+    protected static array $statuses = [
+        'in stock' => 'in stock',
+        'sold out' => 'sold out',
+        'coming soon' => 'coming soon',
+    ];
+
     public static function form(Form $form): Form
     {
         return $form
@@ -29,6 +35,14 @@ class ProductResource extends Resource
                 Forms\Components\TextInput::make('price')
                 ->required()
                 ->rule('numeric'),
+                Forms\Components\Radio::make('status')
+                ->options(self::$statuses),
+                Forms\Components\Select::make('category_id') 
+                    ->relationship('category', 'name'),
+                Forms\Components\Select::make('tags') 
+                    ->relationship('tags', 'name') 
+                    ->multiple(), 
+            
             ]);
     }
 
@@ -38,18 +52,47 @@ class ProductResource extends Resource
             ->columns([
                 Tables\Columns\TextColumn::make('name')
                 ->sortable()
-                ->searchable(isIndividual: true, isGlobal: false), 
+                ->searchable(), 
                 Tables\Columns\TextColumn::make('price')
                 ->sortable()
                 ->money('usd') 
                 ->getStateUsing(function (Product $record): float { 
                     return $record->price / 100; 
                 }), 
+                Tables\Columns\TextColumn::make('status'),
+                Tables\Columns\TextColumn::make('category.name'),
+                Tables\Columns\TextColumn::make('tags.name'),
             ])
             ->defaultSort('price', 'desc')
             ->filters([
-                //
-            ])
+                Tables\Filters\SelectFilter::make('status')
+                    ->options(self::$statuses),
+                Tables\Filters\SelectFilter::make('category')
+                    ->relationship('category', 'name'),
+                Tables\Filters\Filter::make('created_from')
+                    ->form([
+                        Forms\Components\DatePicker::make('created_from'),
+                    ])
+                    ->query(function (Builder $query, array $data): Builder {
+                        return $query
+                            ->when(
+                                $data['created_from'],
+                                fn (Builder $query, $date): Builder => $query->whereDate('created_at', '>=', $date),
+                            );
+                    }),
+                Tables\Filters\Filter::make('created_until')
+                    ->form([
+                        Forms\Components\DatePicker::make('created_until'),
+                    ])
+                    ->query(function (Builder $query, array $data): Builder {
+                        return $query
+                            ->when(
+                                $data['created_until'],
+                                fn (Builder $query, $date): Builder => $query->whereDate('created_at', '<=', $date),
+                            );
+                    }),
+            ], layout: Tables\Enums\FiltersLayout::AboveContent)
+            ->filtersFormColumns(4)
             ->actions([
                 Tables\Actions\EditAction::make(),
                 Tables\Actions\DeleteAction::make(),
@@ -68,7 +111,7 @@ class ProductResource extends Resource
     public static function getRelations(): array
     {
         return [
-            //
+            RelationManagers\TagsRelationManager::class,
         ];
     }
 
